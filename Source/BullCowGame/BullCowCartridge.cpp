768e4136aae63ecf26538c639e5e4d50866361fc
srojasto <s0langel.rt@gmail.com>
2020-04-12 10:17:22 -0600

    Setup game code

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -15,15 +15,14 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 
 void UBullCowCartridge::SetupGame()
 {
-    /*
     HiddenWord = TEXT("cake");
     Lives = HiddenWord.Len();
     bGameOver = false;
-    */
+    
     // Welcoming the player
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    //PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
-    //PrintLine(TEXT("You Have %d lives"), Lives);
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("You Have %d lives"), Lives);
     PrintLine(TEXT("Type on your guess and \npress enter to continue...")); //Prompt for a guess
       
 }
