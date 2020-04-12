a4d50d33aad7762f2e502a099bd4998f488fd296
srojasto <s0langel.rt@gmail.com>
2020-04-12 09:55:56 -0600

    First commit

--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -15,8 +15,12 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 	void SetupGame();
+	void EndGame();
 
 	// Your declarations go below!
 	private:
+	FString HiddenWord;
+	int32 Lives;
+	bool bGameOver;
 	
 };
